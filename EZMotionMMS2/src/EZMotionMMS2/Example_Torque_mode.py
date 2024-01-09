1279b4814d2e0ca11e6206f4601e11a6305ccf9c
Charan-MPS <charan.bhamra@ezmotion.co>
2024-01-08 17:25:35 -0800

    made changes for first release

--- EZMotionMMS2/src/EZMotionMMS2/Example_Torque_mode.py
+++ EZMotionMMS2/src/EZMotionMMS2/Example_Torque_mode.py
@@ -1,21 +1,30 @@
 from .EZMotionMMS2 import *
 import time
-Motor1= Ezm("COM24", 2, 115200)
-Motor1._Shutdown()
-# Motor2= EZMotionMMS2.EZM("COM24", 2, 115200)
-# print("Slave Address for Motor2= ", Motor2._Read_16bit_Reg(0x3050))
-Motor1._Set_Op_Mode("TORQUE")
-Motor1._Set_Target_Torque(-80)
-# Motor1._Set_Acceleration(500)
-# Motor1._Set_Deceleration(500)
-# Motor1._Set_Target_Velocity(-1000)
-Motor1._Enable_Motor()
-print(Motor1._Read_Actual_Torque())
-time.sleep(15)
-print("Motor Speed = ", Motor1._Read_Actual_Velocity())
-print(Motor1._Read_Actual_Torque())
-print(Motor1._Read_Op_Mode())
 
-# time.sleep(5)
-#
-Motor1._Disable_Motor()
+#Create motor object with slave address 1 and port COM24
+Motor1= Ezm("COM24", 1, 115200)
+
+Motor1.Shutdown()
+
+# Set modes of operation to Torque mode
+Motor1.Set_Op_Mode("TORQUE")
+
+#Set target motor torque
+Motor1.Set_Target_Torque(-80)
+
+Motor1.Enable_Motor()
+
+#read and print actual motor torque
+print(Motor1.Read_Actual_Torque())
+time.sleep(15)
+
+#read and print motor actual velocity
+print("Motor Speed = ", Motor1.Read_Actual_Velocity())
+
+#read and print actual motor torque
+print(Motor1.Read_Actual_Torque())
+
+#print selected modes of operation
+print(Motor1.Read_Op_Mode())
+
+Motor1.Disable_Motor()
